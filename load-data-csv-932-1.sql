1798a9a929faafa2c4edf60735fd7956d3c7f213
sworc <lightbox@nifty.com>
2021-11-01 11:15:04 +0900

    Delete load-data-csv-932-1.sql

--- load-data-csv-932-1.sql
+++
@@ -1,8 +0,0 @@
-LOAD DATA INFILE
-    'C:/app/workspace/subject-1008-2/sql-1008/data01.csv'
- INTO TABLE table01
-    FIELDS TERMINATED BY ','
-    OPTIONALLY ENCLOSED BY '"'
-    LINES TERMINATED BY '\r\n';
-
-
