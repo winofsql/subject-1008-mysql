d109e1f18952edbde67e1bae80d19957a642dd7c
sworc <lightbox@nifty.com>
2021-11-01 11:15:15 +0900

    Delete load-data-tsv-932.sql

--- load-data-tsv-932.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    *
- FROM
-    table01
- INTO OUTFILE
-     'C:/app/workspace/subject-1008-2/sql-1008/data03.csv'
-    FIELDS TERMINATED BY ','
-    OPTIONALLY ENCLOSED BY '"'
-    LINES TERMINATED BY '\r\n';
